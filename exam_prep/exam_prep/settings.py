89695ed47fdebea95ee9f8eeb86ad99a845aea1b
Doncho Minkov <DonchoMinkov@gmail.com>
2022-10-25 12:49:51 +0300

    Added result from note about validation

--- exam_prep/exam_prep/settings.py
+++ exam_prep/exam_prep/settings.py
@@ -63,9 +63,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -92,16 +89,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Note about the validation:
+'''
+It does not matter where the validation is made, i.e. either of the following is correct:
+- validators in models
+- validators in forms
+- `clean` and/or `clean_FIELD_NAME` in forms
+'''
